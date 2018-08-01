97a2512e0d4e4271d947e1890064d2a8e8b77b0f
matilde gallina <matilde.gallina@outlook.com>
2018-08-02 00:43:28 +0200

    refactoring nodes table columns

--- Queries/MockMechanicalComponents/SQLQuery - Create database and tables.sql
+++ Queries/MockMechanicalComponents/SQLQuery - Create database and tables.sql
@@ -10,10 +10,27 @@ create table Nodes
 	Name nvarchar(max) not null,
 	SerialCode nvarchar(10) unique not null,
 	ParentId int,
-	Type nvarchar(max) not null
+	Type nvarchar(max) not null,
+	Brand nvarchar(max),
+	Model nvarchar(max),
+	Price decimal(18, 2),
+	FreeMaintenance int,
+	WarrantyPeriod int,
+	Material nvarchar(max),
+	Year int
 )
 
 select * from Nodes
 
 drop table Nodes
 drop database MockMechanicalComponents
+
+UPDATE Nodes
+SET Brand = 'Mercedes'
+WHERE Id = 1
+
+update Nodes
+set Brand = 'Fiat', Model = 'Punto', FreeMaintenance = 1
+where Id = 2
+
+select Brand, Model from Nodes where Id = 1
